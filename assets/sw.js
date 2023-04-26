39f76b47ed6523db3669e9e26cb578a641e43c3a
Aleks Rutins <aleks@rutins.com>
2023-04-26 09:00:44 -0400

    Customize for project name

--- assets/sw.js
+++ assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './typst_desktop.js',
+  './typst_desktop_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
